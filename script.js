dab59a2ed49ae53a16e394fd6f4256a9105a8e02
BeBrandao <beabs2305@gmail.com>
2024-11-22 10:28:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 var andar = [
     "urso-1.png",
     "urso-2.png",
-    "urso-3.png",
-    "urso-4.png",
-    "urso-5.png",
-    "urso-6.png"
+    "urso-3.png"
 ];
 
 var contador = 0;
@@ -13,49 +10,43 @@ var direcao = 1;
 
 function moveRight(){
     var img = document.querySelector("#imagem");
+    img.classList.remove("flip-image");
     
     if (direcao === -1){
         direcao = direcao * -1
     }
-    
-    var indiceImagem = direcao === 1 ? contador % 3 : (contador % 3) + 3;
+
 
     valor = valor + 15 * direcao;
-    contador = contador + 1 * direcao;
+    contador = contador + 1;
 
-    img.src = andar[indiceImagem];
+    img.src = andar[contador % 3];
     img.style.left = valor + "px";
 
-    if (contador === 100) {
-        img.style.left = 0;
-    }
 }
 
 function moveLeft(){
     var img = document.querySelector("#imagem");
+    img.classList.add("flip-image");
     
     if (direcao === 1){
         direcao = direcao * -1
     }
     
-    var indiceImagem = direcao === 1 ? contador % 3 : (contador % 3) + 3;
 
     valor = valor + 15 * direcao;
-    contador = contador + 1 * direcao;
+    contador = contador + 1;
 
-    img.src = andar[indiceImagem];
+    img.src = andar[contador % 3];
     img.style.left = valor + "px";
 
-    if (contador === -1) {
-        img.style.left = 100 + "px";
-    }
 }
 
 function jump(){
     var img = document.querySelector("#imagem");
     img.style.top = 40 + "%"
     setTimeout(() => {
-        img.style.top = "62.5%";
+        img.style.top = "65%";
     }, 350);
 }
 
